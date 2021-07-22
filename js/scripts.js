ef17a871965922a891898b0f1a3df73207faa353
Kiernan2 <kiernan1994@gmail.com>
2021-07-22 13:36:25 -0700

    Fixed

--- js/scripts.js
+++ js/scripts.js
@@ -3,10 +3,14 @@ $(document).ready(function() {
     event.preventDefault();
     let input = ($("input#input").val());
     let words = input.split(" ");
-    words.reverse();
-    words.forEach(function(element) {
-      $("#output").append(element);
-      
+    let filtered_words = [];
+    words.forEach(function(element){
+      if (element.length > 2) {
+        filtered_words.push(element);
+      };
     });
+    filtered_words.reverse();
+    const reInput = filtered_words.join(' ')
+    $("#output").append(reInput);
   });
 });
